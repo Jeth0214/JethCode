6fb66a1267515b0990968a54dc5c627b94e1f936
Jeth0214 <rolandjethrosuyom@yahoo.com>
2023-06-07 18:53:51 +0300

    working on rotating icon menu

--- app/js/menu.js
+++ app/js/menu.js
@@ -1,7 +1,16 @@
+const menu = document.querySelector(".menu");
+const toggler = document.querySelector(".menu-toggler");
 
+
+// This function is for typing text effects
 let typed = new Typed("#auto-type", {
     strings: ["analyze.","develop.", "deploy."],
     typeSpeed: 100,
     backSpeed: 100,
     loop: true
 });
+
+toggler.onclick = () => {
+    // console.log("clicked");
+    menu.classList.toggle("active");
+};
--- dist/menu.js
+++ dist/menu.js
@@ -1,2 +1,2 @@
-"use strict";var typed=new Typed("#auto-type",{strings:["analyze.","develop.","deploy."],typeSpeed:100,backSpeed:100,loop:!0});
+"use strict";var menu=document.querySelector(".menu"),toggler=document.querySelector(".menu-toggler"),typed=new Typed("#auto-type",{strings:["analyze.","develop.","deploy."],typeSpeed:100,backSpeed:100,loop:!0});toggler.onclick=function(){menu.classList.toggle("active")};
 //# sourceMappingURL=menu.js.map
