9a7d404eabb88a7d894a87fc30962069d789eb02
Jeth0214 <rolandjethrosuyom@yahoo.com>
2023-06-09 18:07:40 +0300

    finished circular menu on laptop devices

--- app/js/menu.js
+++ app/js/menu.js
@@ -1,5 +1,10 @@
 const menu = document.querySelector(".menu");
 const toggler = document.querySelector(".menu--hero__toggle");
+const menuItems = document.querySelectorAll(".menu--hero__icon");
+const menuBtn = document.querySelector("#menuBtn");
+
+const menuItemsData = ["About Me", "Projects", "Skills","Testimonials","Contact Me", "Home"];
+let num = 0;
 
 
 // This function is for typing text effects
@@ -15,6 +20,42 @@ toggler.onclick = () => {
     menu.classList.toggle("active");
 };
 
-document.addEventListener("DOMContentLoaded", function(event){
-    menu.classList.toggle("active");
+
+// Animate Circular Menu when the page was loaded
+window.addEventListener("load", function(event){
+    menu.classList.add("active");
+    if(menu.classList.contains("active")){
+        setTimeout( () => {
+            menuBtn.classList.remove("display-n");
+            menuItems[0].children[0].classList.add("active");
+        }, 1000) 
+    }
+});
+
+window.addEventListener("resize", () => {
+    // if (window.innerWidth > 500) {
+    //     navbar.classList.remove("nav--open-menu");
+    //     overlay.classList.remove("overlay");
+    // }
+
+    console.log("resize", window.innerWidth)
+})
+
+  
+
+
+
+  menuItems.forEach( menu => {
+    menu.addEventListener("mouseover", ()=> {
+        let href = menu.children[0].attributes.href.textContent;
+        let menuTitle = href.slice(1).replace("-", " ");
+        menuBtn.textContent = menuTitle;
+        menuBtn.href = href;
+        menu.children[0].classList.add("active");
+        let siblingLinks = [...menuItems].filter(child => child != menu);
+        siblingLinks.forEach(element => { element.children[0].classList.remove("active") });
+    });
   });
+
+  menuBtn.addEventListener("click", () => { menu.classList.remove("active"); })
+
