2ebe0e1cf15f31d3fea2fce460f2fd5b7b027514
Jeth0214 <rolandjethrosuyom@yahoo.com>
2023-06-10 08:20:07 +0300

    working on responsive circular menu

--- app/js/menu.js
+++ app/js/menu.js
@@ -3,59 +3,68 @@ const toggler = document.querySelector(".menu--hero__toggle");
 const menuItems = document.querySelectorAll(".menu--hero__icon");
 const menuBtn = document.querySelector("#menuBtn");
 
-const menuItemsData = ["About Me", "Projects", "Skills","Testimonials","Contact Me", "Home"];
+const menuItemsData = [
+  "About Me",
+  "Projects",
+  "Skills",
+  "Testimonials",
+  "Contact Me",
+  "Home",
+];
 let num = 0;
-
 
 // This function is for typing text effects
 let typed = new Typed("#auto-type", {
-    strings: ["analyze.","develop.", "deploy."],
-    typeSpeed: 100,
-    backSpeed: 100,
-    loop: true
+  strings: ["analyze.", "develop.", "deploy."],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
 });
 
 toggler.onclick = () => {
-    // console.log("clicked");
-    menu.classList.toggle("active");
+  // console.log("clicked");
+  menu.classList.toggle("active");
 };
 
-
 // Animate Circular Menu when the page was loaded
-window.addEventListener("load", function(event){
-    menu.classList.add("active");
-    if(menu.classList.contains("active")){
-        setTimeout( () => {
-            menuBtn.classList.remove("display-n");
-            menuItems[0].children[0].classList.add("active");
-        }, 1000) 
-    }
+window.addEventListener("load", function (event) {
+  menu.classList.add("active");
+  if (menu.classList.contains("active")) {
+    setTimeout(() => {
+      menuBtn.classList.remove("display-n");
+      menuItems[0].children[0].classList.add("active");
+    }, 1000);
+  }
 });
 
 window.addEventListener("resize", () => {
-    // if (window.innerWidth > 500) {
-    //     navbar.classList.remove("nav--open-menu");
-    //     overlay.classList.remove("overlay");
-    // }
+  // if (window.innerWidth > 500) {
+  //     navbar.classList.remove("nav--open-menu");
+  //     overlay.classList.remove("overlay");
+  // }
 
-    console.log("resize", window.innerWidth)
-})
+  console.log("resize", window.innerWidth);
+  if (window.innerWidth < 1169) {
+    menuItems.forEach((menu) => {
+      //   menu.style.left = "-30px";
+    });
+  }
+});
 
-  
-
-
-
-  menuItems.forEach( menu => {
-    menu.addEventListener("mouseover", ()=> {
-        let href = menu.children[0].attributes.href.textContent;
-        let menuTitle = href.slice(1).replace("-", " ");
-        menuBtn.textContent = menuTitle;
-        menuBtn.href = href;
-        menu.children[0].classList.add("active");
-        let siblingLinks = [...menuItems].filter(child => child != menu);
-        siblingLinks.forEach(element => { element.children[0].classList.remove("active") });
+menuItems.forEach((menu) => {
+  menu.addEventListener("mouseover", () => {
+    let href = menu.children[0].attributes.href.textContent;
+    let menuTitle = href.slice(1).replace("-", " ");
+    menuBtn.textContent = menuTitle;
+    menuBtn.href = href;
+    menu.children[0].classList.add("active");
+    let siblingLinks = [...menuItems].filter((child) => child != menu);
+    siblingLinks.forEach((element) => {
+      element.children[0].classList.remove("active");
     });
   });
+});
 
-  menuBtn.addEventListener("click", () => { menu.classList.remove("active"); })
-
+menuBtn.addEventListener("click", () => {
+  menu.classList.remove("active");
+});
