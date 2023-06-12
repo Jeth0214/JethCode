dd1970397699449c90d039592b3831e55dec5742
Jeth0214 <rolandjethrosuyom@yahoo.com>
2023-06-12 06:09:43 +0300

    finished circular menu and started nav toolbar

--- app/js/menu.js
+++ app/js/menu.js
@@ -3,19 +3,9 @@ const toggler = document.querySelector(".menu--hero__toggle");
 const menuItems = document.querySelectorAll(".menu--hero__icon");
 const menuBtn = document.querySelector("#menuBtn");
 
-const menuItemsData = [
-  "About Me",
-  "Projects",
-  "Skills",
-  "Testimonials",
-  "Contact Me",
-  "Home",
-];
-let num = 0;
-
 // This function is for typing text effects
 let typed = new Typed("#auto-type", {
-  strings: ["analyze.", "develop.", "deploy."],
+  strings: ["Analyzing.", "Development.", "Deployment."],
   typeSpeed: 100,
   backSpeed: 100,
   loop: true,
@@ -29,6 +19,7 @@ toggler.onclick = () => {
 // Animate Circular Menu when the page was loaded
 window.addEventListener("load", function (event) {
   menu.classList.add("active");
+  resizeMenu();
   if (menu.classList.contains("active")) {
     setTimeout(() => {
       menuBtn.classList.remove("display-n");
@@ -38,40 +29,44 @@ window.addEventListener("load", function (event) {
 });
 
 window.addEventListener("resize", () => {
-  // if (window.innerWidth > 500) {
-  //     navbar.classList.remove("nav--open-menu");
-  //     overlay.classList.remove("overlay");
-  // }
-  let menuContainer = document.querySelector("#hero-menu-container");
-  console.log("resize", menuContainer.offsetWidth);
-  let width = menuContainer.offsetWidth;
-  if (width < 442) {
-    menuItems.forEach((menu) => {
-      menu.style.transformOrigin = width / 2 + 20 + "px";
-      menu.style.left = width - (width + 20) + "px";
-    });
-  } else {
-    menuItems.forEach((menu) => {
-      menu.style.transformOrigin = 200 + "px";
-      menu.style.left = 0 + "px";
-    });
-  }
+  resizeMenu();
 });
 
-menuItems.forEach((menu) => {
-  menu.addEventListener("mouseover", () => {
-    let href = menu.children[0].attributes.href.textContent;
+menuItems.forEach((menuItem) => {
+  menuItem.addEventListener("mouseover", () => {
+    let href = menuItem.children[0].attributes.href.textContent;
     let menuTitle = href.slice(1).replace("-", " ");
     menuBtn.textContent = menuTitle;
     menuBtn.href = href;
-    menu.children[0].classList.add("active");
-    let siblingLinks = [...menuItems].filter((child) => child != menu);
+    menuItem.children[0].classList.add("active");
+    let siblingLinks = [...menuItems].filter((child) => child != menuItem);
     siblingLinks.forEach((element) => {
       element.children[0].classList.remove("active");
     });
+  });
+  menuItem.children[0].addEventListener("click", (event) => {
+    menu.classList.remove("active");
   });
 });
 
 menuBtn.addEventListener("click", () => {
   menu.classList.remove("active");
 });
+
+function resizeMenu() {
+  let menuContainer = document.querySelector("#hero-menu-container");
+  console.log("resize", menuContainer.offsetWidth);
+  let width = menuContainer.offsetWidth;
+  if (width < 442) {
+    menuItems.forEach((menu) => {
+      menu.style.transformOrigin = width / 2 + 22 + "px";
+      menu.style.left = width - (width + 20) + "px";
+      menu.style.transution = "0.2s";
+    });
+  } else {
+    menuItems.forEach((menu) => {
+      menu.style.transformOrigin = 220 + "px";
+      menu.style.left = 0 + "px";
+    });
+  }
+}
